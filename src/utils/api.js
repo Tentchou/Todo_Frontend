4d985732f1a7bd6d3e96d336ba59a5ea2ce51799
Tentchou nana romeo <tentchouromeo58@gmail.com>
2025-05-28 00:51:24 +0100

    rom

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosCsrf = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL.replace('/api', ''),
   withCredentials: true,
 });
 
